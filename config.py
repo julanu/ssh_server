ae694d42224b68d5de9d447eba1652e68835f71a
jula <marius.jula@yahoo.com>
2020-12-14 14:02:06 +0200

    create skeleton of what will be implemented

---
+++ config.py
@@ -0,0 +1,3 @@
+user = 'test_acc'
+path = '/home/julaw/.ssh/id_rsa'
+machine_ip = '0.0.0.0'
---
+++ remote.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import config as conf
+
+
+def start_service(user, machine_ip, path_to_key):
+    """
+    """
+    pass
+
+
+def stop_service():
+    """
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
